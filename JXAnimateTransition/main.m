9f914387538f42f660356b4f2fa0be412a0b996f
xuweijie_18696781905 <151611438@qq.com>
2015-06-08 18:57:08 +0800

    Initial Commit

---
+++ JXAnimateTransition/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JXAnimateTransition
+//
+//  Created by JackXu on 15/6/8.
+//  Copyright (c) 2015年 BFMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
